592705da54aa879c1e25288c139adb52d5af2520
Nelson Filipe Costa <nelsoncosta.jr17@gmail.com>
2025-02-24 16:31:34 -0500

    organized senses according to levels

--- pre_processing.py
+++ pre_processing.py
@@ -1,9 +1,8 @@
 import os
 import time
+import numpy as np
 import pandas as pd
 import regex as re
-from sklearn.preprocessing import normalize
-from sklearn.model_selection import ShuffleSplit
 
 if not os.path.exists('Data'):
    os.makedirs('Data')
@@ -116,149 +115,82 @@ df_discogem.drop(columns=['MV_dist_de'], inplace=True)
 df_discogem.drop(columns=['MV_dist_fr'], inplace=True)
 df_discogem.drop(columns=['MV_dist_cs'], inplace=True)
 
-df_discogem.to_csv('Data/DiscoGeM-2.0/discogem_2.csv', index=False)
+for label in label_names:
+    for lang in ['_en', '_de', '_fr', '_cs']:
+        df_discogem[label+lang] = df_discogem[label+lang].replace('', np.nan).astype(float)
 
-'''
-
-#####
-
-labels_to_exclude = ['arg1-as-negcond', 'arg2-as-negcond', 'disjunction', 'arg1-as-excpt', 'arg2-as-excpt', 'differentcon', 'norel']
-
-df_discogem.drop(columns=labels_to_exclude, inplace=True)
-
-df_discogem.rename(columns={'arg2-as-subst': 'substitution'}, inplace=True)
-
-for label in labels_to_exclude:
-    label_names.remove(label)
-
-label_names.remove('arg2-as-subst')
-label_names.append('substitution')
-
-df_discogem[label_names] = df_discogem[label_names].astype(float)
-
-df_discogem[label_names] = normalize(df_discogem[label_names], norm='l1', axis=1)
-
-df_discogem['majority_level_3'] = df_discogem[label_names].idxmax(axis=1)
-
-label_names_2 = ['synchronous', 'asynchronous', 'cause', 'condition', 'purpose',
-                 'concession', 'contrast', 'similarity', 'conjunction', 'instantiation',
-                 'level-of-detail', 'equivalence', 'manner', 'substitution']
-
-df_discogem['asynchronous'] = df_discogem['precedence'] + df_discogem['succession']
-df_discogem['cause'] = df_discogem['reason'] + df_discogem['result']
-df_discogem['condition'] = df_discogem['arg1-as-cond'] + df_discogem['arg2-as-cond']
-df_discogem['purpose'] = df_discogem['arg1-as-goal'] + df_discogem['arg2-as-goal']
-df_discogem['concession'] = df_discogem['arg1-as-denier'] + df_discogem['arg2-as-denier']
-df_discogem['instantiation'] = df_discogem['arg1-as-instance'] + df_discogem['arg2-as-instance']
-df_discogem['level-of-detail'] = df_discogem['arg1-as-detail'] + df_discogem['arg2-as-detail']
-df_discogem['manner'] = df_discogem['arg1-as-manner'] + df_discogem['arg2-as-manner']
-
-df_discogem['majority_level_2'] = df_discogem[label_names_2].idxmax(axis=1)
+label_names_2 = ['synchronous', 'asynchronous', 'cause', 'condition', 'neg-condition', 'purpose',
+                 'concession', 'contrast', 'similarity', 'conjunction', 'disjunction', 'equivalence',
+                 'exception', 'instantiation', 'level-of-detail', 'manner', 'substitution']
 
 label_names_1 = ['temporal', 'contingency', 'comparison', 'expansion']
 
-df_discogem['temporal'] = df_discogem['synchronous'] + df_discogem['asynchronous']
-df_discogem['contingency'] = df_discogem['cause'] + df_discogem['condition'] + df_discogem['purpose']
-df_discogem['comparison'] = df_discogem['concession'] + df_discogem['contrast'] + df_discogem['similarity']
-df_discogem['expansion'] = df_discogem['conjunction'] + df_discogem['instantiation'] + df_discogem['level-of-detail'] + df_discogem['equivalence'] + df_discogem['manner'] + df_discogem['substitution']
+for lang in ['_en', '_de', '_fr', '_cs']:
+    # complete level-2 senses
+    df_discogem['asynchronous'+lang]    = df_discogem['precedence'+lang]       + df_discogem['succession'+lang]
+    df_discogem['cause'+lang]           = df_discogem['reason'+lang]           + df_discogem['result'+lang]
+    df_discogem['condition'+lang]       = df_discogem['arg1-as-cond'+lang]     + df_discogem['arg2-as-cond'+lang]
+    df_discogem['neg-condition'+lang]   = df_discogem['arg1-as-negcond'+lang]  + df_discogem['arg2-as-negcond'+lang]
+    df_discogem['purpose'+lang]         = df_discogem['arg1-as-goal'+lang]     + df_discogem['arg2-as-goal'+lang]
+    df_discogem['concession'+lang]      = df_discogem['arg1-as-denier'+lang]   + df_discogem['arg2-as-denier'+lang]
+    df_discogem['exception'+lang]       = df_discogem['arg1-as-excpt'+lang]    + df_discogem['arg2-as-excpt'+lang]
+    df_discogem['instantiation'+lang]   = df_discogem['arg1-as-instance'+lang] + df_discogem['arg2-as-instance'+lang]
+    df_discogem['level-of-detail'+lang] = df_discogem['arg1-as-detail'+lang]   + df_discogem['arg2-as-detail'+lang]
+    df_discogem['manner'+lang]          = df_discogem['arg1-as-manner'+lang]   + df_discogem['arg2-as-manner'+lang]
+    df_discogem['substitution'+lang]    = df_discogem['arg1-as-subst'+lang]    + df_discogem['arg2-as-subst'+lang]
+    # get majority level-2 sense
+    df_discogem['majority_level_2'+lang] = df_discogem[[label+lang for label in label_names_2]].idxmax(axis=1)
+    # complete level-1 senses
+    df_discogem['temporal'+lang]    =  df_discogem['synchronous'+lang]   + df_discogem['asynchronous'+lang]
+    df_discogem['contingency'+lang] = (df_discogem['cause'+lang]         + df_discogem['condition'+lang]     + df_discogem['neg-condition'+lang]   +
+                                       df_discogem['purpose'+lang])
+    df_discogem['comparison'+lang]  =  df_discogem['concession'+lang]    + df_discogem['contrast'+lang]      + df_discogem['similarity'+lang]
+    df_discogem['expansion'+lang]   = (df_discogem['conjunction'+lang]   + df_discogem['disjunction'+lang]   + df_discogem['equivalence'+lang]     +
+                                       df_discogem['exception'+lang]     + df_discogem['instantiation'+lang] + df_discogem['level-of-detail'+lang] +
+                                       df_discogem['manner'+lang]        + df_discogem['substitution'+lang])
+    # get majority level-1 sense
+    df_discogem['majority_level_1'+lang] = df_discogem[[label+lang for label in label_names_1]].idxmax(axis=1)
+    # duplicate level-2 senses without level-3 senses
+    df_discogem['synchronous_3'+lang] = df_discogem['synchronous'+lang]
+    df_discogem['contrast_3'+lang]    = df_discogem['contrast'+lang]
+    df_discogem['similarity_3'+lang]  = df_discogem['similarity'+lang]
+    df_discogem['conjunction_3'+lang] = df_discogem['conjunction'+lang]
+    df_discogem['disjunction_3'+lang] = df_discogem['disjunction'+lang]
+    df_discogem['equivalence_3'+lang] = df_discogem['equivalence'+lang]
 
-df_discogem['majority_level_1'] = df_discogem[label_names_1].idxmax(axis=1)
-
-df_discogem['synchronous_2'] = df_discogem['synchronous']
-df_discogem['contrast_2'] = df_discogem['contrast']
-df_discogem['similarity_2'] = df_discogem['similarity']
-df_discogem['conjunction_2'] = df_discogem['conjunction']
-df_discogem['equivalence_2'] = df_discogem['equivalence']
-df_discogem['substitution_2'] = df_discogem['substitution']
-
-df_discogem['norel'] = 0.0
-df_discogem['norel_1'] = 0.0
-df_discogem['norel_2'] = 0.0
-
-df_discogem = df_discogem[['itemid', 'arg1', 'arg2', 'arg1_arg2', 'majority_level_3', 'synchronous',
-                           'precedence', 'succession', 'reason', 'result', 'arg1-as-cond', 'arg2-as-cond',
-                           'arg1-as-goal', 'arg2-as-goal', 'arg1-as-denier', 'arg2-as-denier', 'contrast',
-                           'similarity', 'conjunction', 'equivalence', 'arg1-as-instance', 'arg2-as-instance',
-                           'arg1-as-detail', 'arg2-as-detail', 'arg1-as-manner', 'arg2-as-manner', 'substitution',
-                           'norel', 'majority_level_2', 'synchronous_2', 'asynchronous', 'cause', 'condition',
-                           'purpose', 'concession', 'contrast_2', 'similarity_2', 'conjunction_2', 'equivalence_2',
-                           'instantiation', 'level-of-detail', 'manner', 'substitution_2', 'norel_2', 'majority_level_1',
-                           'temporal', 'contingency', 'comparison', 'expansion', 'norel_1']]
-
-#####
-
-random_rows = df_discogem.sample(n=600, random_state=17)
-
-df_norel = pd.DataFrame({
-    'itemid': random_rows['itemid'].iloc[0:300].values + '+' + random_rows['itemid'].iloc[300:600].values,
-    'arg1': random_rows['arg1'].iloc[0:300].values,
-    'arg2': random_rows['arg2'].iloc[300:600].values,
-    'arg1_arg2': random_rows['arg1'].iloc[0:300].values + ' ' + random_rows['arg2'].iloc[300:600].values,
-    'majority_level_3': 'norel',
-    'synchronous': 0.0,
-    'precedence': 0.0,
-    'succession': 0.0,
-    'reason': 0.0,
-    'result': 0.0,
-    'arg1-as-cond': 0.0,
-    'arg2-as-cond': 0.0,
-    'arg1-as-goal': 0.0,
-    'arg2-as-goal': 0.0,
-    'arg1-as-denier': 0.0,
-    'arg2-as-denier': 0.0,
-    'contrast': 0.0,
-    'similarity': 0.0,
-    'conjunction': 0.0,
-    'equivalence': 0.0,
-    'arg1-as-instance': 0.0,
-    'arg2-as-instance': 0.0,
-    'arg1-as-detail': 0.0,
-    'arg2-as-detail': 0.0,
-    'arg1-as-manner': 0.0,
-    'arg2-as-manner': 0.0,
-    'substitution': 0.0,
-    'norel': 1.0,
-    'majority_level_2': 'norel',
-    'synchronous_2': 0.0,
-    'asynchronous': 0.0,
-    'cause': 0.0,
-    'condition': 0.0,
-    'purpose': 0.0,
-    'concession': 0.0,
-    'contrast_2': 0.0,
-    'similarity_2': 0.0,
-    'conjunction_2': 0.0,
-    'equivalence_2': 0.0,
-    'instantiation': 0.0,
-    'level-of-detail': 0.0,
-    'manner': 0.0,
-    'substitution_2': 0.0,
-    'norel_2': 1.0,
-    'majority_level_1': 'norel',
-    'temporal': 0.0,
-    'contingency': 0.0,
-    'comparison': 0.0,
-    'expansion': 0.0,
-    'norel_1': 1.0
-})
-
-#####
-
-gs_test = ShuffleSplit(n_splits=1, test_size=0.21, random_state=17)
-train_idx, test_idx = next(gs_test.split(df_discogem, df_discogem['majority_level_3']))
-df_temp = df_discogem.iloc[train_idx]
-df_test = df_discogem.iloc[test_idx]
-
-gs_validation = ShuffleSplit(n_splits=1, test_size=0.125, random_state=16)
-train_idx_discogem, validation_idx_discogem = next(gs_validation.split(df_temp, df_temp['majority_level_3']))
-train_idx_pdtb, validation_idx_pdtb = next(gs_validation.split(df_pdtb, df_pdtb['majority_level_3']))
-df_train = df_temp.iloc[train_idx_discogem]
-df_validation = df_temp.iloc[validation_idx_discogem]
+# reorganize columns
+df_discogem = df_discogem[['split', 'itemid', 'orig_lang', 'available_langs', 'arg1_en', 'arg2_en', 'arg1_arg2_en', 'majority_level_1_en',
+                           'temporal_en', 'contingency_en', 'comparison_en', 'expansion_en', 'majority_level_2_en', 'synchronous_en', 'asynchronous_en',
+                           'cause_en', 'condition_en', 'neg-condition_en', 'purpose_en', 'concession_en', 'contrast_en', 'similarity_en', 'conjunction_en',
+                           'disjunction_en', 'equivalence_en', 'exception_en', 'instantiation_en', 'level-of-detail_en', 'manner_en', 'substitution_en',
+                           'majority_level_3_en', 'synchronous_3_en', 'precedence_en', 'succession_en', 'reason_en', 'result_en', 'arg1-as-cond_en', 'arg2-as-cond_en',
+                           'arg1-as-negcond_en', 'arg2-as-negcond_en', 'arg1-as-goal_en', 'arg2-as-goal_en', 'arg1-as-denier_en', 'arg2-as-denier_en', 'contrast_3_en',
+                           'similarity_3_en', 'conjunction_3_en', 'disjunction_3_en', 'equivalence_3_en', 'arg1-as-excpt_en', 'arg2-as-excpt_en', 'arg1-as-instance_en',
+                           'arg2-as-instance_en', 'arg1-as-detail_en', 'arg2-as-detail_en', 'arg1-as-manner_en', 'arg2-as-manner_en', 'arg1-as-subst_en',
+                           'arg2-as-subst_en', 'norel_en', 'arg1_de', 'arg2_de', 'arg1_arg2_de', 'majority_level_1_de', 'temporal_de', 'contingency_de', 'comparison_de',
+                           'expansion_de', 'majority_level_2_de', 'synchronous_de', 'asynchronous_de', 'cause_de', 'condition_de', 'neg-condition_de', 'purpose_de',
+                           'concession_de', 'contrast_de', 'similarity_de', 'conjunction_de', 'disjunction_de', 'equivalence_de', 'exception_de', 'instantiation_de',
+                           'level-of-detail_de', 'manner_de', 'substitution_de', 'majority_level_3_de', 'synchronous_3_de', 'precedence_de', 'succession_de', 'reason_de',
+                           'result_de', 'arg1-as-cond_de', 'arg2-as-cond_de', 'arg1-as-negcond_de', 'arg2-as-negcond_de', 'arg1-as-goal_de', 'arg2-as-goal_de',
+                           'arg1-as-denier_de', 'arg2-as-denier_de', 'contrast_3_de', 'similarity_3_de', 'conjunction_3_de', 'disjunction_3_de', 'equivalence_3_de',
+                           'arg1-as-excpt_de', 'arg2-as-excpt_de', 'arg1-as-instance_de', 'arg2-as-instance_de', 'arg1-as-detail_de', 'arg2-as-detail_de',
+                           'arg1-as-manner_de', 'arg2-as-manner_de', 'arg1-as-subst_de', 'arg2-as-subst_de', 'norel_de', 'arg1_fr', 'arg2_fr', 'arg1_arg2_fr',
+                           'majority_level_1_fr', 'temporal_fr', 'contingency_fr', 'comparison_fr', 'expansion_fr', 'majority_level_2_fr', 'synchronous_fr',
+                           'asynchronous_fr', 'cause_fr', 'condition_fr', 'neg-condition_fr', 'purpose_fr', 'concession_fr', 'contrast_fr', 'similarity_fr',
+                           'conjunction_fr', 'disjunction_fr', 'equivalence_fr', 'exception_fr', 'instantiation_fr', 'level-of-detail_fr', 'manner_fr', 'substitution_fr',
+                           'majority_level_3_fr', 'synchronous_3_fr', 'precedence_fr', 'succession_fr', 'reason_fr', 'result_fr', 'arg1-as-cond_fr', 'arg2-as-cond_fr',
+                           'arg1-as-negcond_fr', 'arg2-as-negcond_fr', 'arg1-as-goal_fr', 'arg2-as-goal_fr', 'arg1-as-denier_fr', 'arg2-as-denier_fr', 'contrast_3_fr',
+                           'similarity_3_fr', 'conjunction_3_fr', 'disjunction_3_fr', 'equivalence_3_fr', 'arg1-as-excpt_fr', 'arg2-as-excpt_fr', 'arg1-as-instance_fr',
+                           'arg2-as-instance_fr', 'arg1-as-detail_fr', 'arg2-as-detail_fr', 'arg1-as-manner_fr', 'arg2-as-manner_fr', 'arg1-as-subst_fr', 'arg2-as-subst_fr',
+                           'norel_fr', 'arg1_cs', 'arg2_cs', 'arg1_arg2_cs', 'majority_level_1_cs', 'temporal_cs', 'contingency_cs', 'comparison_cs', 'expansion_cs',
+                           'majority_level_2_cs', 'synchronous_cs', 'asynchronous_cs', 'cause_cs', 'condition_cs', 'neg-condition_cs', 'purpose_cs', 'concession_cs',
+                           'contrast_cs', 'similarity_cs', 'conjunction_cs', 'disjunction_cs', 'equivalence_cs', 'exception_cs', 'instantiation_cs', 'level-of-detail_cs',
+                           'manner_cs', 'substitution_cs', 'majority_level_3_cs', 'synchronous_3_cs', 'precedence_cs', 'succession_cs', 'reason_cs', 'result_cs',
+                           'arg1-as-cond_cs', 'arg2-as-cond_cs', 'arg1-as-negcond_cs', 'arg2-as-negcond_cs', 'arg1-as-goal_cs', 'arg2-as-goal_cs', 'arg1-as-denier_cs',
+                           'arg2-as-denier_cs', 'contrast_3_cs', 'similarity_3_cs', 'conjunction_3_cs', 'disjunction_3_cs', 'equivalence_3_cs', 'arg1-as-excpt_cs',
+                           'arg2-as-excpt_cs', 'arg1-as-instance_cs', 'arg2-as-instance_cs', 'arg1-as-detail_cs', 'arg2-as-detail_cs', 'arg1-as-manner_cs',
+                           'arg2-as-manner_cs', 'arg1-as-subst_cs', 'arg2-as-subst_cs', 'norel_cs']]
 
 df_discogem.to_csv('Data/DiscoGeM-2.0/discogem_2.csv', index=False)
-df_train.to_csv('Data/DiscoGeM-2.0/discogem_2_train.csv', index=False)
-df_validation.to_csv('Data/DiscoGeM-2.0/discogem_2_validation.csv', index=False)
-df_test.to_csv('Data/DiscoGeM-2.0/discogem_2_test.csv', index=False)
 
 print(f'Completed in {(time.time()-start_time)/60:.2f} minutes.')
-'''
