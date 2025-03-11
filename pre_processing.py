f6e6170ecca56a3251f758ab47ce9ce33daab329
Nelson Filipe Costa <nelsoncosta.jr17@gmail.com>
2025-03-11 14:35:20 -0400

    removed unnecessary spacing

--- pre_processing.py
+++ pre_processing.py
@@ -33,7 +33,7 @@ discogem_columns = ['split',
                     'MV_cs',
                     'MV_dist_cs']
 
-df_discogem = pd.read_csv('Corpora/DiscoGeM-2.0/DiscoGeM-2.0_corpus/DiscoGeM-2.0_items.csv', usecols = discogem_columns, delimiter='\t', dtype=str)
+df_discogem = pd.read_csv('Corpora/DiscoGeM-2.0/DiscoGeM-2.0_corpus/DiscoGeM-2.0_items.csv', usecols=discogem_columns, delimiter='\t', dtype=str)
 
 df_discogem['arg1_arg2_en'] = df_discogem['arg1_context_en'].copy() + ' ' + df_discogem['arg2_context_en'].copy()
 df_discogem['arg1_arg2_de'] = df_discogem['arg1_context_de'].copy() + ' ' + df_discogem['arg2_context_de'].copy()
