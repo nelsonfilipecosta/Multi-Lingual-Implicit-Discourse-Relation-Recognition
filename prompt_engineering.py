603f9662a9b6817262cc0ca34de72e84b6e47c24
Nelson Filipe Costa <nelsoncosta.jr17@gmail.com>
2025-04-13 18:11:17 -0400

    added if main

--- prompt_engineering.py
+++ prompt_engineering.py
@@ -24,6 +24,7 @@ client = openai.OpenAI()
 def create_prompt(main_prompt, main_response, examples_number=0):
     messages = [{"role": "user",      "content": main_prompt},
                 {"role": "assistant", "content": main_response}]
+
     if examples_number > 0:
         for i in range(examples_number):
             messages.append({"role": "user",      "content": examples["question"][i]})
@@ -31,48 +32,52 @@ def create_prompt(main_prompt, main_response, examples_number=0):
 
     return messages
 
+
 def create_question(arg_1, arg_2):
     question = f"Sentence 1: {arg_1}\n"\
                f"Sentence 2: {arg_2}"
-    
+
     return question
 
+
 def prompt_llm(model, messages):
-    response = client.chat.completions.create(model=model,
+    response = client.chat.completions.create(model = model,
                                               messages = messages,
-                                              temperature=0,
-                                              max_tokens=1024)
-    
+                                              temperature = 0,
+                                              max_tokens = 1024)
+
     return response.choices[0].message.content
 
 
-if MODE == "validation":
-    df = pd.read_csv("Data/DiscoGeM-2.0/discogem_2_single_lang_" + LANG + "_validation.csv")
-elif MODE == "test":
-    df = pd.read_csv("Data/DiscoGeM-2.0/discogem_2_single_lang_" + LANG + "_test.csv")
+if __name__ == "__main__":
 
-prompt_path = "Prompts/main_prompt_" + LANG + ".txt"
-response_path = "Prompts/main_response_" + LANG + ".txt"
+    if MODE == "validation":
+        df = pd.read_csv("Data/DiscoGeM-2.0/discogem_2_single_lang_" + LANG + "_validation.csv")
+    elif MODE == "test":
+        df = pd.read_csv("Data/DiscoGeM-2.0/discogem_2_single_lang_" + LANG + "_test.csv")
 
-arg_1 = df["arg1"].tolist()
-arg_2 = df["arg2"].tolist()
-labels = df.iloc[:,32:60].values.tolist()
+    prompt_path = "Prompts/main_prompt_" + LANG + ".txt"
+    response_path = "Prompts/main_response_" + LANG + ".txt"
 
-if LANG == "en":
-    from Prompts.examples import examples_en as examples
+    arg_1 = df["arg1"].tolist()
+    arg_2 = df["arg2"].tolist()
+    labels = df.iloc[:,32:60].values.tolist()
 
-for i in range(3):
-    main_prompt = open(prompt_path).read()
-    main_response=  open(response_path).read()
-    question = create_question(arg_1[i], arg_2[i])
+    if LANG == "en":
+        from Prompts.examples import examples_en as examples
 
-    messages = create_prompt(main_prompt, main_response, EXAMPLES_NUMBER)
-    messages.append({"role": "user", "content": question})
-    print()
-    print(messages)
-    print()
+    for i in range(3):
+        main_prompt = open(prompt_path).read()
+        main_response=  open(response_path).read()
+        question = create_question(arg_1[i], arg_2[i])
 
-    response = prompt_llm("gpt-4o-mini", messages)
+        messages = create_prompt(main_prompt, main_response, EXAMPLES_NUMBER)
+        messages.append({"role": "user", "content": question})
+        print()
+        print(messages)
+        print()
 
-    print(response)
-    print()
+        response = prompt_llm("gpt-4o-mini", messages)
+
+        print(response)
+        print()
