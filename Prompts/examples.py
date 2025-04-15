34b5acfffd05feb13000f1aae8b40d5e2a25732f
Nelson Filipe Costa <nelsoncosta.jr17@gmail.com>
2025-04-15 16:41:21 -0400

    added llm prompts to repository

---
+++ Prompts/examples.py
@@ -0,0 +1,133 @@
+examples_all = {
+    "question": [
+        "Sentence 1: Everybody seemed to hear a rustling outside the door. There was no sound of footsteps.\n"\
+        "Sentence 2: It was like light silk sliding over the panel. Then it stopped.",
+
+        "Sentence 1: It pretends to respect human rights. It pretends to persecute no one.\n"\
+        "Sentence 2: It pretends to fear nothing. It pretends to pretend nothing.",
+
+        "Sentence 1: Siebeneinhalb war ich. Susi zählte vielleicht neun.\n"\
+        "Sentence 2: Klein-Käschen war knapp acht. Axel, Nuchi, Hänschen und Harry zehn oder elf.",
+
+        "Sentence 1: Mais, Arsène Lupin? N'y a-t-il pas mieux à faire?\n"\
+        "Sentence 2: Voyons, procédons par ordre. Après tout, ta conscience est tranquille...",
+
+        "Sentence 1: Někdo je venku! ''Bilbo ucítil srdce až v hrdle.\n"\
+        "Sentence 2: Ze vší síly sebou zavrtěl. Knoflíky se rozletěly na všechny strany.",
+    ],
+    "answer": [
+        "[0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.5]",
+        "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.286 0.143, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.143, 0.429, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.0, 0.0, 0.0, 0.125, 0.0, 0.25, 0.0, 0.0, 0.0, 0.125, 0.0, 0.0, 0.0, 0.125, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.125, 0.125, 0.125, 0.0, 0.0, 0.0]",
+        "[0.0, 0.286, 0.0, 0.0, 0.143, 0.143, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.286, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.143, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+    ]
+}
+
+
+examples_en = {
+    "question": [
+        "Sentence 1: I tried even not to resist. Besides, I did not have the strength.\n"\
+        "Sentence 2: My temples throbbed; I was almost strangled. One minute more, and I would have breathed my last.",
+
+        "Sentence 1: Silence. Not even a tiny sound.\n"\
+        "Sentence 2: The colonel turned red like a rooster. He sent the eleventh company back and commanded them to do it again as he had ordered.",
+
+        "Sentence 1: Everybody seemed to hear a rustling outside the door. There was no sound of footsteps.\n"\
+        "Sentence 2: It was like light silk sliding over the panel. Then it stopped.",
+
+        "Sentence 1: It pretends to respect human rights. It pretends to persecute no one.\n"\
+        "Sentence 2: It pretends to fear nothing. It pretends to pretend nothing.",
+
+        "Sentence 1: Then she lay flat on the raft, while I trod water. After a moment she turned and looked at me.\n"\
+        "Sentence 2: Her hair was over her eyes and she was laughing. I clambered up on to the raft, beside her."
+    ],
+    "answer": [
+        "[0.0, 0.0, 0.0, 0.111, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.556, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.333, 0.0, 0.0, 0.0, 0.0]",
+        "[0.1, 0.5, 0.0, 0.0, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.5]",
+        "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 0.0, 0.0, 0.0, 0.0]"
+    ]
+}
+
+
+examples_de = {
+    "question": [
+        "Sentence 1: Dumme Schokolade sollte mich ködern. Mama hielt sie und machte einen spitzen Mund dabei.\n"\
+        "Sentence 2: Matzerath war es, der mit gemachter Strenge nach meinem invaliden Instrument griff. Ich klammerte mich an das Wrack.",
+
+        "Sentence 1: Kannst ja doch nichts damit anfangen. Biste viel zu dumm für und zu klein.\n"\
+        "Sentence 2: Wirste noch kaputtmachen. Hat über sechs Gulden gekostet.",
+
+        "Sentence 1: Siebeneinhalb war ich. Susi zählte vielleicht neun.\n"\
+        "Sentence 2: Klein-Käschen war knapp acht. Axel, Nuchi, Hänschen und Harry zehn oder elf.",
+
+        "Sentence 1: Auf Krümel? Mama und Jan Bronski ließen kein Krümelchen übrig.\n"\
+        "Sentence 2: Die aßen alles selbst auf. Die hatten den großen Appetit, der nie aufhört, der sich selbst in den Schwanz beißt.",
+
+        "Sentence 1: Haben Sie Ihre zweite Amtsperiode schon einbezogen? Aber jetzt im Ernst.\n"\
+        "Sentence 2: Die sich erweiternde Union braucht Stärkung durch Verschlankung und Begrenzung. Erstens, Verschlankung."
+    ],
+    "answer": [
+        "[0.333, 0.333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.222, 0.111, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.0, 0.0, 0.0, 0.111, 0.667, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.222, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.286 0.143, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.143, 0.429, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.0, 0.0, 0.0, 0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0]",
+        "[0.0, 0.0, 0.0, 0.0, 0.333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.333, 0.0, 0.0, 0.0, 0.0]"
+    ]
+}
+
+
+examples_fr = {
+    "question": [
+        "Sentence 1: Mais, Arsène Lupin? N'y a-t-il pas mieux à faire?\n"\
+        "Sentence 2: Voyons, procédons par ordre. Après tout, ta conscience est tranquille...",
+
+        "Sentence 1: Ah! Mais ils font trop de musique ces gens.\n"\
+        "Sentence 2: L'autre jour j'ai rencontré le père Vinteuil près du cimetière. Il ne tenait pas sur ses jambes.",
+
+        "Sentence 1: Quelle différence avec Forcheville! Voilà au moins un homme qui vous dit carrément sa façon de penser.\n"\
+        "Sentence 2: Ça vous plaît ou ça ne vous plaît pas. Ce n'est pas comme l'autre qui n'est jamais ni figue ni raisin.",
+
+        "Sentence 1: Il alla chez Odette. Il s'assit loin d'elle.\n"\
+        "Sentence 2: Il n'osait l'embrasser, ne sachant si en elle, si en lui, c'était l'affection ou la colère qu'un baiser réveillerait. Il se taisait, il regardait mourir leur amour.",
+
+        "Sentence 1: On ne voit pas le fantôme! Et il n'a ni habit ni tête! ...\n"\
+        "Sentence 2: Tout ce qu'on a raconté sur sa tête de mort et sur sa tête de feu, c'est des blagues! Il n'a rien du tout..."
+    ],
+    "answer": [
+        "[0.0, 0.0, 0.0, 0.125, 0.0, 0.25, 0.0, 0.0, 0.0, 0.125, 0.0, 0.0, 0.0, 0.125, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.125, 0.125, 0.125, 0.0, 0.0, 0.0]",
+        "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.091, 0.0, 0.0, 0.091, 0.0, 0.091, 0.091, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.273, 0.0, 0.0, 0.091, 0.0, 0.182,, 0.091, 0.0, 0.0, 0.0, 0.0]",
+        "[0.1, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.1, 0.0, 0.0, 0.0]",
+        "[0.0, 0.0, 0.0, 0.1, 0.2, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0]"
+    ]
+}
+
+
+examples_cs = {
+    "question": [
+        "Sentence 1: Připadalo mu, že už prošel celým zítřkem do pozítřka a ještě dál. Najednou bez jakéhokoli varovného znamení šlápl s cáknutím do vody!\n"\
+        "Sentence 2: Brr, byla studená jako led. Teď nastražil všechny smysly.",
+
+        "Sentence 1: Dostal z ní taky pořádný hlad. Tentokrát se ani nevrátil do člunu.\n"\
+        "Sentence 2: Usadil se ve tmě vedle Bilba. To bylo hobitovi příšerně nepříjemné a rozptylovalo mu to myšlenky.",
+
+        "Sentence 1: Někdo je venku! ''Bilbo ucítil srdce až v hrdle.\n"\
+        "Sentence 2: Ze vší síly sebou zavrtěl. Knoflíky se rozletěly na všechny strany.",
+
+        "Sentence 1: Pǐedstírá, že respektuje lidská práva. Pǐedstírá, že nikoho nepronásleduje.\n"\
+        "Sentence 2: Pǐedstírá, že se nebojí. Pǐedstírá, že nic nepǐedstírá.",
+
+        "Sentence 1: Byli spolu sotva hodinu. Doprovázela ho na nádraží a čekala s ním až do chvíle, kdy nasedl do vlaku.\n"\
+        "Sentence 2: O deset dnů později přijela za nim do Prahy. Milovali se spolu ještě téhož dne."
+    ],
+    "answer": [
+        "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.333, 0.0, 0.333, 0.0, 0.0, 0.333, 0.0, 0.0]",
+        "[0.167, 0.333, 0.0, 0.0, 0.167, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.333, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.0, 0.286, 0.0, 0.0, 0.143, 0.143, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.286, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.143, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.286, 0.143, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.571, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]",
+        "[0.0, 0.571, 0.0, 0.143, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.143, 0.0, 0.0, 0.143, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
+    ]
+}
